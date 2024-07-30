40c32344af88a3ff316e4f3d458a14995aa13cc5
KseniiaMarkiv <ksenya.markiv@gmail.com>
2024-07-30 16:45:08 -0400

    Fixed error with the Google API was unable to use an address we gave it

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -21,12 +21,18 @@ contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
 
-  legislators = civic_info.representative_info_by_address(
-    address: zipcode,
-    levels: 'country',
-    roles: ['legislatorUpperBody', 'legislatorLowerBody']
-  )
-  legislators = legislators.officials
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    )
+    legislators = legislators.officials
+  rescue
+    puts
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+    puts
+  end
 
   puts "#{name} #{zipcode} #{legislators}"
 end
