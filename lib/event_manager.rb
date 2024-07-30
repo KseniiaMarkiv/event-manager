bdd7695175f0141df5a002afa02e48d3f0ffb2ad
KseniiaMarkiv <ksenya.markiv@gmail.com>
2024-07-30 16:48:06 -0400

    Moving displaying legislators to a method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,12 +1,29 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 
-civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-civic_info.key = File.read('secret.key').strip
-
 # common methods
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = File.read('secret.key').strip
+
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    )
+    legislators = legislators.officials
+    legislator_names = legislators.map(&:name)
+    legislator_names.join(", ")
+  rescue
+    puts
+    p 'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+    puts
+  end
 end
 
 puts 'EventManager initialized.'
@@ -21,21 +38,7 @@ contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
 
-  begin
-    legislators = civic_info.representative_info_by_address(
-      address: zipcode,
-      levels: 'country',
-      roles: ['legislatorUpperBody', 'legislatorLowerBody']
-    )
-    legislators = legislators.officials
+  legislators = legislators_by_zipcode(zipcode)
 
-    legislator_names = legislators.map(&:name)
-    legislators_string = legislator_names.join(", ")
-  rescue
-    puts
-    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
-    puts
-  end
-
-  puts "#{name} #{zipcode} #{legislators_string}"
+  puts "#{name} #{zipcode} #{legislators}"
 end
